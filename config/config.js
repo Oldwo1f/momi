34360ddf09cd718e3dc1cc10a40102f51bc01499
Oldwo1f <alexismomcilovic@gmail.com>
2016-06-15 13:01:02 +0200

    Delete config.js

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  company: 'nutrimaketing',
-  secret: 'cd7d35bdb480141b0acf03d04dd29146',
-  port: 1337,
-  mainEmail : 'alexismomcilovic@gmail.com',
-  mainEmailPassword : 'Alexis09',
-  esName : 'momi',
-};
